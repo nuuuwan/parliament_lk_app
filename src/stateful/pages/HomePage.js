0d84689d41a95316fde3d5756cb1d99a272abd4a
nuuuwan <nuuuwan@gmail.com>
2022-05-05 17:31:28 +0530

    HomePage

--- src/stateful/pages/HomePage.js
+++ src/stateful/pages/HomePage.js
@@ -1,10 +1,11 @@
 import { Component } from "react";
-
+import MPListView from "../molecules/MPListView.js";
 export default class HomePage extends Component {
   render() {
     return (
       <div>
-        <div>HomePage</div>
+        <h1>Parliament of Sri Lanka</h1>
+        <MPListView />
       </div>
     );
   }
