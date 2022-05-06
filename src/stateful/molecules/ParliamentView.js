a174d1eb248835d37ee53a6d00fb0fb619bcf9ae
nuuuwan <nuuuwan@gmail.com>
2022-05-06 14:49:04 +0530

    Added Dim metadata

--- src/stateful/molecules/ParliamentView.js
+++ src/stateful/molecules/ParliamentView.js
@@ -3,6 +3,17 @@ import { Component } from "react";
 import MP from "../../core/MP.js";
 import MPWidget from "../../nonstate/molecules/MPWidget.js";
 import GridView from "../../nonstate/molecules/GridView.js";
+import DimensionPicker from "../../nonstate/atoms/DimensionPicker.js";
+
+const DEFAULT_X_DIM = "Party";
+const DEFAULT_Y_DIM = "Age Group";
+
+const DIM_TO_FUNC = {
+  "Age Group": (mp) => mp.getAgeGroup(10),
+  Party: (mp) => mp.party,
+  "National List": (mp) => mp.isNationalList,
+  "Electoral District": (mp) => mp.electoralDistrict,
+};
 
 function buildGrid(dataList, xMap, yMap, cellMap) {
   const dxyList = dataList.map(function (d) {
@@ -16,6 +27,7 @@ function buildGrid(dataList, xMap, yMap, cellMap) {
   const xAxisLabels = DataStructuresFuture.unique(
     dxyList.map((dxy) => dxy.x)
   ).sort();
+
   const yAxisLabels = DataStructuresFuture.unique(
     dxyList.map((dxy) => dxy.y)
   ).sort();
@@ -41,7 +53,11 @@ function buildGrid(dataList, xMap, yMap, cellMap) {
 export default class ParliamentView extends Component {
   constructor(props) {
     super(props);
-    this.state = { mpList: undefined };
+    this.state = {
+      mpList: undefined,
+      xDim: DEFAULT_X_DIM,
+      yDim: DEFAULT_Y_DIM,
+    };
   }
 
   async componentDidMount() {
@@ -50,24 +66,29 @@ export default class ParliamentView extends Component {
   }
 
   render() {
-    const { mpList } = this.state;
+    const { mpList, xDim, yDim } = this.state;
     if (mpList === undefined) {
-      return "Loading...";
+      return <div>Loading...</div>;
     }
 
     const { cells, xAxisLabels, yAxisLabels } = buildGrid(
       mpList,
-      (mp) => mp.party,
-      (mp) => mp.getAgeGroup(10),
-      (mp) => <MPWidget mp={mp} />
+      DIM_TO_FUNC[xDim],
+      DIM_TO_FUNC[yDim],
+      (mp) => <MPWidget key={`mp-${mp.id}`} mp={mp} />
     );
 
     return (
-      <GridView
-        cells={cells}
-        xAxisLabels={xAxisLabels}
-        yAxisLabels={yAxisLabels}
-      />
+      <div>
+        <DimensionPicker selectedDimension={xDim} />
+        {" x "}
+        <DimensionPicker selectedDimension={yDim} />
+        <GridView
+          cells={cells}
+          xAxisLabels={xAxisLabels}
+          yAxisLabels={yAxisLabels}
+        />
+      </div>
     );
   }
 }
