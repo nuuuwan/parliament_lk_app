4942f74907c2f6ac710076983516cf4cc54d03a9
nuuuwan <nuuuwan@gmail.com>
2022-05-06 19:15:22 +0530

    Added AppBar

--- src/stateful/pages/HomePage.js
+++ src/stateful/pages/HomePage.js
@@ -1,9 +1,27 @@
 import { Component } from "react";
+import * as React from "react";
+
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
 import ParliamentView from "../molecules/ParliamentView.js";
+
+const STYLE = {
+  backgroundColor: "lightgray",
+};
+
 export default class HomePage extends Component {
   render() {
     return (
       <div>
+        <AppBar position="static" sx={STYLE}>
+          <Toolbar variant="dense">
+            <Typography variant="h6" component="div">
+              Parliament of Sri Lanka
+            </Typography>
+          </Toolbar>
+        </AppBar>
         <ParliamentView />
       </div>
     );
