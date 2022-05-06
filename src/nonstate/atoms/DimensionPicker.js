04730d8edeee66d729f9c613ac10f153aed688e9
nuuuwan <nuuuwan@gmail.com>
2022-05-06 15:49:09 +0530

    Added STYLE

--- src/nonstate/atoms/DimensionPicker.js
+++ src/nonstate/atoms/DimensionPicker.js
@@ -1,5 +1,12 @@
 import { DIMENSION_LIST } from "../../core/Dimensions.js";
 
+const STYLE = {
+  padding: 6,
+  backgroundColor: "ghost",
+  borderRadius: 6,
+  borderStyle: "none",
+};
+
 export default function DimensionPicker(props) {
   const { selectedDimension, onChange } = props;
 
@@ -7,7 +14,11 @@ export default function DimensionPicker(props) {
     return onChange(e.target.value);
   };
   return (
-    <select defaultValue={selectedDimension} onChange={onChangeInner}>
+    <select
+      defaultValue={selectedDimension}
+      onChange={onChangeInner}
+      style={STYLE}
+    >
       {DIMENSION_LIST.map(function (dimension, iDimension) {
         const key = `option-${iDimension}`;
         return (
