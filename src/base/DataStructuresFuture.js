c09db0fd7af08d1817a3a64de6e4d151e7cd8293
nuuuwan <nuuuwan@gmail.com>
2022-05-06 13:41:23 +0530

    Lint only

--- src/base/DataStructuresFuture.js
+++ src/base/DataStructuresFuture.js
@@ -1,22 +1,16 @@
 export default class DataStructuresFuture {
   static initArray(n, funcDefaultValue) {
-    return [...Array(n).keys()].map(i => funcDefaultValue(i));
+    return [...Array(n).keys()].map((i) => funcDefaultValue(i));
   }
 
   static initArray2D(nX, nY, funcDefaultValue) {
-    return DataStructuresFuture.initArray(
-      nX,
-      iX => DataStructuresFuture.initArray(
-          nY,
-          iY => funcDefaultValue(iX, iY),
-      )
+    return DataStructuresFuture.initArray(nX, (iX) =>
+      DataStructuresFuture.initArray(nY, (iY) => funcDefaultValue(iX, iY))
     );
   }
 
   static range(min, max) {
     const span = max - min;
-    return DataStructuresFuture.initArray(span, i => i + min);
+    return DataStructuresFuture.initArray(span, (i) => i + min);
   }
-
-
 }
