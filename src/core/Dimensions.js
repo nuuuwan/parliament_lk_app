469093acc8a705dcdd8932d3fd08d4e307144269
nuuuwan <nuuuwan@gmail.com>
2022-05-06 15:26:52 +0530

    Moved Dimensions logic

---
+++ src/core/Dimensions.js
@@ -0,0 +1,57 @@
+import DataStructuresFuture from "../base/DataStructuresFuture.js";
+
+export const DIMENSION_TO_FUNC = {
+  "Age Group": (mp) => mp.getAgeGroup(10),
+  Party: (mp) => mp.party,
+  "Is National List": (mp) => mp.isNationalList,
+  "Electoral District": (mp) => mp.electoralDistrict,
+  Religion: (mp) => mp.religion,
+  Profession: (mp) => mp.profession,
+  "Is Sinhala Buddhist": (mp) => mp.isSinhalaBuddhist,
+};
+
+export const DIMENSION_LIST = Object.keys(DIMENSION_TO_FUNC);
+
+export default class Dimensions {
+  static buildGrid(dataList, xMap, yMap, cellMap) {
+    const dxyList = dataList.map(function (d) {
+      return {
+        d,
+        x: xMap(d),
+        y: yMap(d),
+      };
+    });
+
+    const xAxisLabelAndCount = DataStructuresFuture.keyAndCount(
+      dxyList.map((dxy) => dxy.x)
+    );
+
+    const yAxisLabelAndCount = DataStructuresFuture.keyAndCount(
+      dxyList.map((dxy) => dxy.y)
+    );
+
+
+
+    const xAxisLabels = xAxisLabelAndCount.map(x => x[0]);
+    const yAxisLabels = yAxisLabelAndCount.map(x => x[0]);
+
+
+    const nX = xAxisLabels.length;
+    const nY = yAxisLabels.length;
+
+    const xToIX = DataStructuresFuture.buildReverseIndex(xAxisLabels);
+    const yToIY = DataStructuresFuture.buildReverseIndex(yAxisLabels);
+
+    const cells = dxyList.reduce(
+      function (cells, dxy) {
+        const [iX, iY] = [xToIX[dxy.x], yToIY[dxy.y]];
+        cells[iX][iY].push(cellMap(dxy.d));
+        return cells;
+      },
+      DataStructuresFuture.initArray2D(nX, nY, (iX, iY) => [])
+    );
+
+    return { cells, xAxisLabels, yAxisLabels };
+  }
+
+}
