77bfecf161e6d2a35f58aa5ee6e87353b3f53aa9
nuuuwan <nuuuwan@gmail.com>
2022-05-06 15:27:13 +0530

    Use Dimensions

--- src/stateful/molecules/ParliamentView.js
+++ src/stateful/molecules/ParliamentView.js
@@ -1,57 +1,17 @@
-import DataStructuresFuture from "../../base/DataStructuresFuture.js";
 import { Component } from "react";
 import MP from "../../core/MP.js";
 import MPWidget from "../../nonstate/molecules/MPWidget.js";
 import GridView from "../../nonstate/molecules/GridView.js";
 import DimensionPicker from "../../nonstate/atoms/DimensionPicker.js";
 
+import Dimensions, {DIMENSION_TO_FUNC} from '../../core/Dimensions.js';
+
 const DEFAULT_X_DIM = "Party";
 const DEFAULT_Y_DIM = "Age Group";
 
-const DIM_TO_FUNC = {
-  "Age Group": (mp) => mp.getAgeGroup(10),
-  Party: (mp) => mp.party,
-  "Is National List": (mp) => mp.isNationalList,
-  "Electoral District": (mp) => mp.electoralDistrict,
-  Religion: (mp) => mp.religion,
-  Profession: (mp) => mp.profession,
-  "Is Sinhala Buddhist": (mp) => mp.isSinhalaBuddhist,
-};
 
-function buildGrid(dataList, xMap, yMap, cellMap) {
-  const dxyList = dataList.map(function (d) {
-    return {
-      d,
-      x: xMap(d),
-      y: yMap(d),
-    };
-  });
 
-  const xAxisLabels = DataStructuresFuture.unique(
-    dxyList.map((dxy) => dxy.x)
-  ).sort();
 
-  const yAxisLabels = DataStructuresFuture.unique(
-    dxyList.map((dxy) => dxy.y)
-  ).sort();
-
-  const nX = xAxisLabels.length;
-  const nY = yAxisLabels.length;
-
-  const xToIX = DataStructuresFuture.buildReverseIndex(xAxisLabels);
-  const yToIY = DataStructuresFuture.buildReverseIndex(yAxisLabels);
-
-  const cells = dxyList.reduce(
-    function (cells, dxy) {
-      const [iX, iY] = [xToIX[dxy.x], yToIY[dxy.y]];
-      cells[iX][iY].push(cellMap(dxy.d));
-      return cells;
-    },
-    DataStructuresFuture.initArray2D(nX, nY, (iX, iY) => [])
-  );
-
-  return { cells, xAxisLabels, yAxisLabels };
-}
 
 export default class ParliamentView extends Component {
   constructor(props) {
@@ -82,10 +42,10 @@ export default class ParliamentView extends Component {
       return <div>Loading...</div>;
     }
 
-    const { cells, xAxisLabels, yAxisLabels } = buildGrid(
+    const { cells, xAxisLabels, yAxisLabels } = Dimensions.buildGrid(
       mpList,
-      DIM_TO_FUNC[xDim],
-      DIM_TO_FUNC[yDim],
+      DIMENSION_TO_FUNC[xDim],
+      DIMENSION_TO_FUNC[yDim],
       (mp) => <MPWidget key={`mp-${mp.id}`} mp={mp} />
     );
 
