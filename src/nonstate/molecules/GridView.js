fee3faca812b03d52b1fcf6f3067efa4418c7842
nuuuwan <nuuuwan@gmail.com>
2022-05-06 15:49:15 +0530

    Updated STYLEs

--- src/nonstate/molecules/GridView.js
+++ src/nonstate/molecules/GridView.js
@@ -1,31 +1,32 @@
 import PctWidget from "../atoms/PctWidget.js";
 
+const STYLE_GRID = {
+  marginTop: 32,
+  borderCollapse: "collapse",
+  tableLayout: "fixed",
+};
+
+const STYLE_CELL = {
+  borderStyle: "solid",
+  borderWidth: 1,
+  borderColor: "lightgray",
+  overflow: "hidden",
+  textAlign: "center",
+  fontSize: 9,
+};
+
 export default function GridView(props) {
   const { cells, xAxisLabels, yAxisLabels } = props;
 
-  const styleGrid = {
-    borderCollapse: "collapse",
-    tableLayout: "fixed",
-  };
-
-  const styleCellCustom = {
-    borderStyle: "solid",
-    borderWidth: 1,
-    borderColor: "lightgray",
-    overflow: "hidden",
-    textAlign: "center",
-    fontSize: 9,
-  };
-
   return (
-    <table style={styleGrid}>
+    <table style={STYLE_GRID}>
       <tbody>
         <tr>
           <th />
           {xAxisLabels.map(function (xLabel, iX) {
             const key = `x-label-${iX}`;
             return (
-              <th key={key} style={styleCellCustom}>
+              <th key={key} style={STYLE_CELL}>
                 {xLabel}
               </th>
             );
@@ -36,13 +37,13 @@ export default function GridView(props) {
           const key = `row-${iY}`;
           return (
             <tr key={key}>
-              {<th style={styleCellCustom}>{yLabel}</th>}
+              {<th style={STYLE_CELL}>{yLabel}</th>}
               {xAxisLabels.map(function (xLabel, iX) {
                 const key = `cell-${iX}-${iY}`;
                 const cellContents = cells[iX][iY];
                 const n = cellContents.length;
                 return (
-                  <td key={key} style={styleCellCustom}>
+                  <td key={key} style={STYLE_CELL}>
                     {cellContents}
                     <PctWidget n={n} />
                   </td>
