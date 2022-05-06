569d147857060dc66c44de5b2e3272528487547e
nuuuwan <nuuuwan@gmail.com>
2022-05-06 23:24:05 +0530

    More Reference Links

--- src/nonstate/molecules/MPDrawerView.js
+++ src/nonstate/molecules/MPDrawerView.js
@@ -2,15 +2,111 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
 import HomeIcon from "@mui/icons-material/Home";
 import PhoneIcon from "@mui/icons-material/Phone";
 import EmailIcon from "@mui/icons-material/Email";
+import GavelIcon from "@mui/icons-material/Gavel";
 import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
+import TravelExploreIcon from "@mui/icons-material/TravelExplore";
+
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
 
 const STYLE_BOX = { padding: 3 };
 const STYLE_AVATAR = { width: 100, height: 100 };
+
+function Address({ address, isSitting }) {
+  if (!address) {
+    return null;
+  }
+  const href =
+    "https://www.google.com/maps/search/" + address.replace(" ", "+");
+  const secondaryText = isSitting ? "Sitting" : "";
+  return (
+    <ListItem disablePadding>
+      <ListItemButton component="a" href={href}>
+        <ListItemIcon>
+          <HomeIcon color="disabled" />
+        </ListItemIcon>
+        <ListItemText primary={address} secondary={secondaryText} />
+      </ListItemButton>
+    </ListItem>
+  );
+}
+
+function Phone({ phone }) {
+  if (!phone) {
+    return null;
+  }
+  const href = "tel:" + phone;
+
+  return (
+    <ListItem disablePadding>
+      <ListItemButton component="a" href={href}>
+        <ListItemIcon>
+          <PhoneIcon color="disabled" />
+        </ListItemIcon>
+        <ListItemText primary={phone} />
+      </ListItemButton>
+    </ListItem>
+  );
+}
+
+function Email({ email }) {
+  if (!email) {
+    return null;
+  }
+
+  const href = "mailto:" + email;
+  return (
+    <ListItem disablePadding>
+      <ListItemButton component="a" href={href}>
+        <ListItemIcon>
+          <EmailIcon color="disabled" />
+        </ListItemIcon>
+        <ListItemText primary={email} />
+      </ListItemButton>
+    </ListItem>
+  );
+}
+
+function Wikipedia({ searchText }) {
+  if (!searchText) {
+    return null;
+  }
+
+  const href =
+    "https://en.wikipedia.org/w/index.php?search=" +
+    searchText.replace(" ", "+");
+  return (
+    <ListItem disablePadding>
+      <ListItemButton component="a" href={href}>
+        <ListItemIcon>
+          <TravelExploreIcon color="disabled" />
+        </ListItemIcon>
+        <ListItemText primary={"Wikipedia"} />
+      </ListItemButton>
+    </ListItem>
+  );
+}
+
+function Pariament({ id }) {
+  const href = "https://www.parliament.lk/component/members/viewMember/" + id;
+  return (
+    <ListItem disablePadding>
+      <ListItemButton component="a" href={href}>
+        <ListItemIcon>
+          <GavelIcon color="disabled" />
+        </ListItemIcon>
+        <ListItemText primary={"Parliment.LK"} />
+      </ListItemButton>
+    </ListItem>
+  );
+}
 
 export default function MPDrawerView(props) {
   const { mp } = props;
@@ -33,40 +129,28 @@ export default function MPDrawerView(props) {
         {mp.party + " - " + mp.electoralDistrict}
       </Typography>
 
-      <Typography variant="subtitle1" display="block">
-        <HomeIcon />
-        {mp.addressSitting}
-      </Typography>
-      <Typography variant="subtitle1" display="block">
-        <PhoneIcon />
-        <Link href={"tel:" + mp.phoneSitting} variant="body2" underline="hover">
-          {mp.phoneSitting}
-        </Link>
-      </Typography>
+      <List>
+        <Phone phone={mp.phoneSitting} />
 
-      <Typography variant="subtitle1" display="block">
-        <HomeIcon />
-        {mp.address}
-      </Typography>
-      <Typography variant="subtitle1" display="block">
-        <PhoneIcon />
-        <Link href={"tel:" + mp.phone} variant="body2" underline="hover">
-          {mp.phone}
-        </Link>
-      </Typography>
+        <Address address={mp.addressSitting} isSitting={true} />
 
-      <Typography variant="subtitle1" display="block">
-        <EmailIcon />
-        <Link href={"mailto:" + mp.email} variant="body2" underline="hover">
-          {mp.email}
-        </Link>
-      </Typography>
+        <Phone phone={mp.phone} />
+
+        <Address address={mp.address} isSitting={false} />
+
+        <Email email={mp.email} />
+      </List>
 
       <Stack direction="row" spacing={1}>
         <Chip label={mp.civilStatus} variant="outlined" />
         <Chip label={mp.religion} variant="outlined" />
         <Chip label={parseInt(mp.age) + " years"} variant="outlined" />
       </Stack>
+
+      <List>
+        <Wikipedia searchText={mp.name} />
+        <Pariament id={mp.id} />
+      </List>
     </Box>
   );
 }
