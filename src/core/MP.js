acbab67a6c6f087521d2f56da2b9cadb28915fe2
nuuuwan <nuuuwan@gmail.com>
2022-05-07 13:01:13 +0530

    Updated properties to use new expanded_mp_list;

--- src/core/MP.js
+++ src/core/MP.js
@@ -4,106 +4,50 @@ import TimeXFuture, { SECONDS_IN } from "../base/TimeXFuture.js";
 const URL_MP_LIST =
   "https://raw.githubusercontent.com/" +
   "nuuuwan/parliament_lk/data/" +
-  "mp_list.json";
+  "expanded_mp_list.json";
 
 export default class MP {
   constructor(d) {
     this.d = d;
 
-    this.urlNum = parseInt(d.url_num);
-    this.nameData = d.name;
+    this.id = parseInt(d.id);
+    this.name = d.name_cleaned;
+    this.firstNames = d.first_names
+    this.lastName = d.last_name
+    this.gender = d.gender;
     this.imageURL = d.image_url;
-    this.partyData = d.party;
-    this.electoralDistrict = d.electoral_district;
+    this.party = d.party_short;
+    this.edName = d.ed_name;
+    this.provinceName = d.province_name;
 
-    this.dateOfBirthData = d.date_of_birth;
+    this.dateOfBirth = d.date_of_birth_norm;
+    this.dateOfBirthUT = d.date_of_birth_ut;
+
     this.civilStatus = d.civil_status;
-    this.religionData = d.religion;
+    this.religion = d.religion_cleaned;
+    
     this.profession = d.profession;
-    this.phone = d.phone;
-
+    this.phone = d.phone_norm;
     this.address = d.address;
-    this.phoneSitting = d.phone_sitting;
+    this.phoneSitting = d.phone_sitting_norm;
     this.addressSitting = d.address_sitting;
     this.email = d.email;
+
+    this.academicQualifications = d.academic_qualifications
+    this.academicHighestLevel = d.academic_highest_level
+
     this.sourceURL = d.source_url;
   }
 
-  get id() {
-    return this.urlNum;
-  }
-
-  get gender() {
-    if (
-      this.nameData.includes("Mrs.") ||
-      this.nameData.includes("Miss.") ||
-      this.nameData.includes("Ms.")
-    ) {
-      return "Female";
-    }
-    return "Male";
-  }
-
-  get name() {
-    return this.nameData
-      .replace("Hon.", "")
-      .replace(", M.P.", "")
-      .replace("(Dr.)", "")
-      .replace("(Mrs.)", "")
-      .replace("(Major)", "")
-      .replace("Field Marshal", "")
-      .replace("(Ms.)", "")
-      .replace(", PC", "")
-      .replace("Thero", "")
-      .replace("(Ven.) ", "")
-      .trim();
-  }
-
-  get firstNames() {
-    const tokens = this.name.split(" ");
-    if (tokens.slice(-2, -1).join(" ").toLowerCase() === "de") {
-      return tokens.slice(0, -2).join(" ");
-    }
-    return tokens.slice(0, -1).join(" ");
-  }
-
-  get lastName() {
-    const tokens = this.name.split(" ");
-    if (tokens.slice(-2, -1).join(" ").toLowerCase() === "de") {
-      return "De " + tokens.slice(-1);
-    }
-    return tokens.slice(-1);
-  }
-
-  get party() {
-    return this.partyData.split("(")[1].split(")")[0];
-  }
-
-  get dateOfBirth() {
-    if (this.dateOfBirthData) {
-      return this.dateOfBirthData;
-    }
-    const CUSTOM_DATE_OF_BIRTH = {
-      1575: "27-04-1951", // Basil Rohana Rajapaksa
-      3451: "01-10-1971", // Jagath Kumara Sumithraarachchi
-      3364: "07-10-1974", // Lalith Varna Kumara
-    };
-    if (CUSTOM_DATE_OF_BIRTH[this.id]) {
-      return CUSTOM_DATE_OF_BIRTH[this.id];
-    }
-    console.warn("No dateOfBirth for " + this.name + this.id);
-    return "01-01-1970";
-  }
 
   get age() {
     const utNow = TimeX.getUnixTime();
-    const utDateOfBirth = TimeXFuture.parse(this.dateOfBirth);
-    const age = (utNow - utDateOfBirth) / SECONDS_IN.YEAR;
+    const age = (utNow - this.dateOfBirthUT) / SECONDS_IN.YEAR;
     return age;
   }
 
   get isNationalList() {
-    return this.electoralDistrict === "National List"
+    return this.edName === "National List"
       ? "National List"
       : "Electoral Districts";
   }
@@ -120,17 +64,6 @@ export default class MP {
       return `Age > ${ageLimit}`;
     }
     return `Age ≤ ${ageLimit}`;
-  }
-
-  get religion() {
-    if (["Roman Catholicism", "Christianity"].includes(this.religionData)) {
-      return "Christianity (All)";
-    }
-
-    if (!this.religionData || this.religionData === "Other") {
-      return "Other or Unknown";
-    }
-    return this.religionData;
   }
 
   get isSinhalaBuddhist() {
