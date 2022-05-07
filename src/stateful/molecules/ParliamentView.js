3a655052989556fdfbaef16a2cd9382f50a48416
nuuuwan <nuuuwan@gmail.com>
2022-05-07 16:43:38 +0530

    ToolTip, showStatisticalTrends

--- src/stateful/molecules/ParliamentView.js
+++ src/stateful/molecules/ParliamentView.js
@@ -3,6 +3,9 @@ import { Component } from "react";
 import Drawer from "@mui/material/Drawer";
 import Box from "@mui/material/Box";
 import Avatar from "@mui/material/Avatar";
+import Checkbox from '@mui/material/Checkbox';
+import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
 
 import MP from "../../core/MP.js";
 import GridView from "../../nonstate/molecules/GridView.js";
@@ -11,12 +14,22 @@ import DimensionPicker from "../../nonstate/atoms/DimensionPicker.js";
 
 import Dimensions from "../../core/Dimensions.js";
 
-const DEFAULT_X_DIM = "Religion";
-const DEFAULT_Y_DIM = "Party";
+const DEFAULT_X_DIM = "Is Age > 40";
+const DEFAULT_Y_DIM = "Gender";
 
 const STYLE = {
   margin: 2,
 };
+
+const STATISTICAL_TRENDS_TOOLTOP = [
+  'Statistical Trends measure if certain grid cells contain more MPs than',
+  ' we would expect if they were assigned at random. ',
+  ' The "z" (e.g. x = 2.5) value shows how many standard deviations',
+  ' the number of MPs vary from the mean number of expected MPs.',
+  ' The range (e.g. Exp. 84 to 114) is the 95% confidence interval for the',
+  ' number of MPs. If the actual number is outside this range,',
+  ' the probability that the trend is random is <5%.',
+].join('')
 
 export default class ParliamentView extends Component {
   constructor(props) {
@@ -26,6 +39,7 @@ export default class ParliamentView extends Component {
       xDim: DEFAULT_X_DIM,
       yDim: DEFAULT_Y_DIM,
       activeMPId: null,
+      showStatisticalTrends: false,
     };
   }
 
@@ -45,13 +59,19 @@ export default class ParliamentView extends Component {
     this.setState({ activeMPId: null });
   }
 
+  onShowStatisticalTrendsClick() {
+    const oldState = this.state.showStatisticalTrends;
+
+    this.setState({showStatisticalTrends: !oldState});
+  }
+
   async componentDidMount() {
     const mpIdx = await MP.getMPIdx();
     this.setState({ mpIdx });
   }
 
   render() {
-    const { mpIdx, xDim, yDim, activeMPId } = this.state;
+    const { mpIdx, xDim, yDim, activeMPId, showStatisticalTrends } = this.state;
     if (mpIdx === undefined) {
       return <div>Loading...</div>;
     }
@@ -92,11 +112,22 @@ export default class ParliamentView extends Component {
           selectedDimension={xDim}
           onChange={this.onChangeXDim.bind(this)}
         />
+
+
+        <Checkbox checked={showStatisticalTrends} onClick={this.onShowStatisticalTrendsClick.bind(this)}/>
+        <Tooltip title={STATISTICAL_TRENDS_TOOLTOP}>
+          <Typography variant="caption">
+            Show Statistical Trends
+          </Typography>
+        </Tooltip>
+
+
         <GridView
           cells={cells}
           xAxisLabels={xAxisLabels}
           yAxisLabels={yAxisLabels}
           onClick={this.onClickMP}
+          showStatisticalTrends={showStatisticalTrends}
         />
         <Drawer
           anchor="right"
