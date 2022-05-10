5c2d2c830720ca9ac200d79652ae1f0c2e55d907
nuuuwan <nuuuwan@gmail.com>
2022-05-10 17:32:12 +0530

    Added isSorted

--- src/core/Dimension.js
+++ src/core/Dimension.js
@@ -2,8 +2,10 @@ export default class Dimension {
   constructor(
       name,
       func,
+      isSorted=false,
   ) {
-    this.name = name
-    this.func = func
+    this.name = name;
+    this.func = func;
+    this.isSorted = isSorted;
   }
 }
