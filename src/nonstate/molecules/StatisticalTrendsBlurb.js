48d79d32632f90a0548d8101acb51a174e9037c4
nuuuwan <nuuuwan@gmail.com>
2022-05-11 15:06:23 +0530

    Added getHumanText

--- src/nonstate/molecules/StatisticalTrendsBlurb.js
+++ src/nonstate/molecules/StatisticalTrendsBlurb.js
@@ -3,6 +3,16 @@ import { t } from "../../base/I18N.js";
 
 const COLOR_SWITCH_ON = "#1976D2";
 
+function getHumanText(z) {
+  const absZ = Math.abs(z);
+  if (absZ < 2) {
+    return "";
+  }
+  let directionStr = z > 0 ? "more" : "fewer";
+  let magnitudeStr = absZ > 4 ? "Highly" : "";
+  return directionStr + " Significantly " + magnitudeStr;
+}
+
 export default function StatisticalTrendsBlurb({
   countXY,
   countX,
@@ -13,24 +23,7 @@ export default function StatisticalTrendsBlurb({
   const meanCount = n * p;
   const stdevCount = Math.sqrt(n * p * (1 - p));
   const z = (count - meanCount) / stdevCount;
-  const absZ = Math.abs(z);
-  let humanText = "";
-
-  if (absZ > 1) {
-    if (z >= 4) {
-      humanText = "Highly significantly more";
-    } else if (z <= -4) {
-      humanText = "Highly significantly fewer";
-    } else if (z >= 2) {
-      humanText = "Significantly more";
-    } else if (z <= -2) {
-      humanText = "Significantly fewer";
-    } else if (z >= 1) {
-      humanText = "Slightly more";
-    } else if (z <= -1) {
-      humanText = "Slightly fewer";
-    }
-  }
+  const humanText = getHumanText(z);
 
   const lowerCount = parseInt((meanCount - stdevCount * 2) * 10 + 0.5) / 10;
   const upperCount = parseInt((meanCount + stdevCount * 2) * 10 + 0.5) / 10;
