a5a3017aea24eb58597471275aa1c48d5f171a00
nuuuwan <nuuuwan@gmail.com>
2022-05-12 14:42:08 +0530

    Added disableTranslate

--- src/nonstate/molecules/TitledChip.js
+++ src/nonstate/molecules/TitledChip.js
@@ -2,17 +2,23 @@ import Typography from "@mui/material/Typography";
 import { t } from "../../base/I18N.js";
 import ChipOuter, { STYLE_TITLE, STYLE_BODY } from "./ChipOuter.js";
 
-export default function TitledChip({ title, body, titleAtBottom }) {
+export default function TitledChip({
+  title,
+  body,
+  titleAtBottom,
+  disableTranslate,
+}) {
   if (!body) {
     return null;
   }
-
+  
   const renderedTitle = <Typography sx={STYLE_TITLE}>{t(title)}</Typography>;
 
+  const renderedBody = disableTranslate ? body : t(body);
   return (
     <ChipOuter>
       {titleAtBottom ? null : renderedTitle}
-      <Typography sx={STYLE_BODY}>{t(body)}</Typography>
+      <Typography sx={STYLE_BODY}>{renderedBody}</Typography>
       {titleAtBottom ? renderedTitle : null}
     </ChipOuter>
   );
