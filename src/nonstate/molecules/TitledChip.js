94410cd75552e0830c30be905fff8fc981bd8842
nuuuwan <nuuuwan@gmail.com>
2022-05-12 11:53:40 +0530

    Don't render anything, if body == null

--- src/nonstate/molecules/TitledChip.js
+++ src/nonstate/molecules/TitledChip.js
@@ -3,6 +3,9 @@ import { t } from "../../base/I18N.js";
 import ChipOuter, { STYLE_TITLE, STYLE_BODY } from "./ChipOuter.js";
 
 export default function TitledChip({ title, body }) {
+  if (!body) {
+    return null;
+  }
   return (
     <ChipOuter>
       <Typography sx={STYLE_TITLE}>{t(title)}</Typography>
