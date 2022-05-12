667c13f3df2c32f567258523df054ef05d1495cc
nuuuwan <nuuuwan@gmail.com>
2022-05-12 11:41:12 +0530

    Use ChipOuter

--- src/nonstate/atoms/ElectoralRegionBlurb.js
+++ src/nonstate/atoms/ElectoralRegionBlurb.js
@@ -1,17 +1,18 @@
 import Typography from "@mui/material/Typography";
 import { t } from "../../base/I18N.js";
+import ChipOuter, { STYLE_TITLE, STYLE_BODY } from "../molecules/ChipOuter.js";
 
 export default function ElectoralRegionBlurb({ mp }) {
   return (
-    <>
-      <Typography variant="body1" display="block">
-        {t(mp.edName)}
-      </Typography>
+    <ChipOuter>
+      <Typography sx={STYLE_BODY}>{t(mp.edName)}</Typography>
       {mp.edName === "National List" ? null : (
-        <Typography variant="body2" display="block">
-          {t(mp.provinceName)} {t("Province")}
-        </Typography>
+        <>
+          <Typography sx={STYLE_TITLE}>{t("Electoral District")}</Typography>
+          <Typography sx={STYLE_BODY}>{t(mp.provinceName)}</Typography>
+          <Typography sx={STYLE_TITLE}>{t("Province")}</Typography>
+        </>
       )}
-    </>
+    </ChipOuter>
   );
 }
