4bd897edd123d062e4c610e22e6f3802df7927aa
nuuuwan <nuuuwan@gmail.com>
2022-05-12 11:41:35 +0530

    Use STYLEs from ChipOuter

--- src/nonstate/molecules/TitledChip.js
+++ src/nonstate/molecules/TitledChip.js
@@ -1,14 +1,12 @@
 import Typography from "@mui/material/Typography";
 import { t } from "../../base/I18N.js";
-import ChipOuter from "./ChipOuter.js";
+import ChipOuter, { STYLE_TITLE, STYLE_BODY } from "./ChipOuter.js";
 
 export default function TitledChip({ title, body }) {
   return (
     <ChipOuter>
-      <Typography sx={{ fontSize: "x-small", color: "gray" }}>
-        {t(title)}
-      </Typography>
-      <Typography sx={{ fontSize: "normal" }}>{t(body)}</Typography>
+      <Typography sx={STYLE_TITLE}>{t(title)}</Typography>
+      <Typography sx={STYLE_BODY}>{t(body)}</Typography>
     </ChipOuter>
   );
 }
