8eb0e8ce88f8f1ce56cb4c06d9916b2b4b7fc2ae
nuuuwan <nuuuwan@gmail.com>
2022-05-12 10:48:45 +0530

    Added Political Parties; Added ElectoralRegionBlurb;

--- src/nonstate/molecules/MPDrawer.js
+++ src/nonstate/molecules/MPDrawer.js
@@ -1,10 +1,14 @@
 import * as React from "react";
 
 import Box from "@mui/material/Box";
-
 import ProfileHeader from "./ProfileHeader.js";
+import Typography from "@mui/material/Typography";
+
 import CloseButton from "../atoms/CloseButton.js";
+import ElectoralRegionBlurb from "../atoms/ElectoralRegionBlurb.js";
 import MPDrawerGroup from "./MPDrawerGroup.js";
+
+import { t } from "../../base/I18N.js";
 
 const STYLE_BOX = { padding: 3, maxWidth: "75%" };
 
@@ -19,12 +23,18 @@ export default function MPDrawer(props) {
       <CloseButton onClose={onClose} />
       <ProfileHeader mp={mp} />
 
-      <MPDrawerGroup name="Political Parties" />
+      <MPDrawerGroup name="Political Parties">
+        <Typography variant="body1" display="block">
+          {t(mp.party)}
+        </Typography>
+      </MPDrawerGroup>
       <MPDrawerGroup name="Education & Profession" />
       <MPDrawerGroup name="Voting & Parliamentary Attandance" />
       <MPDrawerGroup name="Transparency & Corruption" />
       <MPDrawerGroup name="Demographics" />
-      <MPDrawerGroup name="Electoral Regions" />
+      <MPDrawerGroup name="Electoral Regions">
+        <ElectoralRegionBlurb mp={mp} />
+      </MPDrawerGroup>
       <MPDrawerGroup name="Religion & Ethnicity" />
     </Box>
   );
