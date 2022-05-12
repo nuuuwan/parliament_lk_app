cb1a752279fc6e12e96bc648225a93199e42163a
nuuuwan <nuuuwan@gmail.com>
2022-05-12 12:15:51 +0530

    Support titleAtBottom

--- src/nonstate/molecules/TitledChip.js
+++ src/nonstate/molecules/TitledChip.js
@@ -2,14 +2,18 @@ import Typography from "@mui/material/Typography";
 import { t } from "../../base/I18N.js";
 import ChipOuter, { STYLE_TITLE, STYLE_BODY } from "./ChipOuter.js";
 
-export default function TitledChip({ title, body }) {
+export default function TitledChip({ title, body, titleAtBottom }) {
   if (!body) {
     return null;
   }
+
+  const renderedTitle = <Typography sx={STYLE_TITLE}>{t(title)}</Typography>;
+
   return (
     <ChipOuter>
-      <Typography sx={STYLE_TITLE}>{t(title)}</Typography>
+      {titleAtBottom ? null : renderedTitle}
       <Typography sx={STYLE_BODY}>{t(body)}</Typography>
+      {titleAtBottom ? renderedTitle : null}
     </ChipOuter>
   );
 }
