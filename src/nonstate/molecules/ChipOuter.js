9dc7976f66497a58629b3e256342689df277c4bf
nuuuwan <nuuuwan@gmail.com>
2022-05-12 11:41:02 +0530

    Added STYLE constants

--- src/nonstate/molecules/ChipOuter.js
+++ src/nonstate/molecules/ChipOuter.js
@@ -6,6 +6,9 @@ const STYLE = {
   margin: "1%",
 };
 
+export const STYLE_TITLE = { fontSize: "x-small", color: "gray" };
+export const STYLE_BODY = { fontSize: "normal" };
+
 export default function ChipOuter({ children }) {
   return <div style={STYLE}>{children}</div>;
 }
