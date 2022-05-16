a9e52e7bf52a06a2789b6ef8185723896ece90ce
nuuuwan <nuuuwan@gmail.com>
2022-05-16 12:19:55 +0530

    Updated ChipOuter Styles

--- src/nonstate/molecules/ChipOuter.js
+++ src/nonstate/molecules/ChipOuter.js
@@ -1,13 +1,14 @@
 const STYLE = {
   backgroundColor: "#f8f8f8",
-  padding: "3%",
-  borderRadius: "20%",
+  padding: "1%",
+  borderRadius: "5%",
   display: "inline-block",
   margin: "1%",
+  maxWidth: 100,
 };
 
-export const STYLE_TITLE = { fontSize: "x-small", color: "gray" };
-export const STYLE_BODY = { fontSize: "normal" };
+export const STYLE_TITLE = { fontSize: "xx-small", color: "gray" };
+export const STYLE_BODY = { fontSize: "small" };
 
 export default function ChipOuter({ children }) {
   return <div style={STYLE}>{children}</div>;
