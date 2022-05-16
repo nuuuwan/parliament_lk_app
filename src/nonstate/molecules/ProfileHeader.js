d90fe2a39f06997d1105b845bb90368a9e733024
nuuuwan <nuuuwan@gmail.com>
2022-05-16 15:04:56 +0530

    Added Halo Color

--- src/nonstate/molecules/ProfileHeader.js
+++ src/nonstate/molecules/ProfileHeader.js
@@ -2,13 +2,22 @@ import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
 
 import { t } from "../../base/I18N.js";
+import { PARTY_IDX } from "../../core/Party.js";
 
-const STYLE_AVATAR = { width: 100, height: 100 };
+const AVATAR_SIZE = 100;
 
 export default function ProfileHeader({ mp }) {
+  const color = PARTY_IDX[mp.party];
+  const style = {
+    width: AVATAR_SIZE,
+    height: AVATAR_SIZE,
+    borderColor: color,
+    borderWidth: AVATAR_SIZE / 10,
+    borderStyle: "solid",
+  };
   return (
     <div style={{ margin: 1 }}>
-      <Avatar alt={mp.name} src={mp.imageURL} sx={STYLE_AVATAR} />
+      <Avatar alt={mp.name} src={mp.imageURL} sx={style} />
       <Typography variant="subtitle2" display="block">
         {t(mp.firstNames)}
       </Typography>
