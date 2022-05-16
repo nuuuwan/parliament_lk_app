453dbccb4ba3039dcf346686adb16455675925db
nuuuwan <nuuuwan@gmail.com>
2022-05-16 19:29:53 +0530

    Added DimPanel

---
+++ src/nonstate/molecules/DimPanel.js
@@ -0,0 +1,32 @@
+import Grid from "@mui/material/Grid";
+import IconButton from "@mui/material/IconButton";
+import SwapHorizIcon from "@mui/icons-material/SwapHoriz";
+
+import DimPicker from "../../nonstate/atoms/DimPicker.js";
+
+
+export default function DimPanel({
+    xDim,
+    yDim,
+    onChangeYDim,
+    onClickSwapDims,
+    onChangeXDim,
+}) {
+
+  return (
+    <Grid container justifyContent="center">
+      <DimPicker
+        selectedDim={yDim}
+        onChange={onChangeYDim}
+      />
+
+      <IconButton onClick={onClickSwapDims}>
+        <SwapHorizIcon />
+      </IconButton>
+      <DimPicker
+        selectedDim={xDim}
+        onChange={onChangeXDim}
+      />
+    </Grid>
+  );
+}
