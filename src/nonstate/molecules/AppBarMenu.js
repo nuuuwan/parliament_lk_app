b8f51c1c4b7d48d3e1a54a8b98666c863b4ef9b0
nuuuwan <nuuuwan@gmail.com>
2022-05-16 15:55:32 +0530

    Added Help; Added Icons

--- src/nonstate/molecules/AppBarMenu.js
+++ src/nonstate/molecules/AppBarMenu.js
@@ -3,16 +3,31 @@ import ReactGA from "react-ga";
 import IconButton from "@mui/material/IconButton";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemIcon from "@mui/material/ListItemIcon";
 import MenuIcon from "@mui/icons-material/Menu";
 import TravelExploreIcon from "@mui/icons-material/TravelExplore";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import GavelIcon from "@mui/icons-material/Gavel";
+import HelpIcon from "@mui/icons-material/Help";
 
 import { t } from "../../base/I18N.js";
 
 const MENU_ITEM_LIST = [
   {
-    name: "@ParliamentLK",
+    name: "Help",
+    url: "https://twitter.com/nuuuwan/status/1522912345256865795",
+    details: "Help, Examples and Usage",
+    Icon: HelpIcon,
+  },
+  {
+    name: "Code",
+    url: "http://github.com/nuuuwan",
+    details: "Visualization, Design and App by @nuuuwan",
+    Icon: GitHubIcon,
+  },
+  {
+    name: "Data - The Parliament of Sri Lanka",
     url:
       "https://www.parliament.lk/" +
       "en/members-of-parliament/directory-of-members/",
@@ -20,16 +35,10 @@ const MENU_ITEM_LIST = [
     Icon: GavelIcon,
   },
   {
-    name: "@TISriLanka",
+    name: "Data - Transparency International - Sri Lanka",
     url: "https://www.tisrilanka.org/mpassets/",
     details: "Asset declaration data is from www.tisrilanka.org",
     Icon: TravelExploreIcon,
-  },
-  {
-    name: "@nuuuwan",
-    url: "http://github.com/nuuuwan",
-    details: "Visualization, Design and App by @nuuuwan",
-    Icon: GitHubIcon,
   },
 ];
 
@@ -67,6 +76,7 @@ export default function AppBarMenu() {
       >
         {MENU_ITEM_LIST.map(function (menuItem, i) {
           const key = "app-bar-menu-item-" + i;
+          const Icon = menuItem.Icon;
           const onClick = function (e) {
             ReactGA.event({
               category: "External Links",
@@ -80,7 +90,10 @@ export default function AppBarMenu() {
 
           return (
             <MenuItem key={key} onClick={onClick}>
-              {t(menuItem.name)}
+              <ListItemIcon>
+                <Icon />
+              </ListItemIcon>
+              <ListItemText>{t(menuItem.name)}</ListItemText>
             </MenuItem>
           );
         })}
