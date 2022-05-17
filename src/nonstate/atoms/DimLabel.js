2db29cf86dad8cd7054483f9131862e953ecf5c1
nuuuwan <nuuuwan@gmail.com>
2022-05-17 07:22:23 +0530

    Added STYLE, increased size, and made gray

--- src/nonstate/atoms/DimLabel.js
+++ src/nonstate/atoms/DimLabel.js
@@ -1,6 +1,11 @@
 import Typography from "@mui/material/Typography";
 import { t } from "../../base/I18N.js";
 
+const STYLE = {
+  fontSize: "xx-small",
+  color: "gray",
+};
+
 export default function DimLabel({ dim }) {
-  return <Typography sx={{ fontSize: "xx-small" }}>{t(dim)}</Typography>;
+  return <Typography sx={STYLE}>{t(dim)}</Typography>;
 }
