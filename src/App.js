e2a61c3356e6aff669a916c8b45a64d71d26f1b2
nuuuwan <nuuuwan@gmail.com>
2022-05-18 13:14:28 +0530

    Cleaned up to make tests pass;

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactGA from "react-ga";
 import HomePage from "./stateful/pages/HomePage.js";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import "./App.css";
 
-const TRACKING_ID = "UA-228120741-2";
-ReactGA.initialize(TRACKING_ID);
+export default class App extends Component {
+  render() {
+    try {
+      ReactGA.initialize("UA-228120741-2"); // HACK: Unittests
+    } catch {}
 
-function App() {
-  const theme = createTheme({
-    typography: {
-      fontFamily: ["PT Sans", "sans-serif"].join(","),
-      fontSize: 14,
-    },
-  });
+    const theme = createTheme({
+      typography: {
+        fontFamily: ["PT Sans", "sans-serif"].join(","),
+        fontSize: 14,
+      },
+    });
 
-  return (
-    <ThemeProvider theme={theme}>
-      <HomePage />
-    </ThemeProvider>
-  );
+    return (
+      <ThemeProvider theme={theme}>
+        <HomePage />
+      </ThemeProvider>
+    );
+  }
 }
-
-export default App;
